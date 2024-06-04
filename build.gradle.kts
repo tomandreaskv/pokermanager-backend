52929b0d5172784f7992440afea4fdcbc13b9915
Tom Andreas Krane Vingås <tom.andreas@vingaas.no>
2024-06-04 18:53:50 +0200

    added changed to use env variables in docker compose and in flyway init config and db in general

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("jvm") version "1.9.24"
+	id("org.asciidoctor.jvm.convert") version "3.3.2"
 	kotlin("plugin.spring") version "1.9.24"
 	kotlin("plugin.jpa") version "1.9.24"
 }
@@ -19,19 +20,25 @@ repositories {
 	mavenCentral()
 }
 
+extra["snippetsDir"] = file("build/generated-snippets")
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:postgresql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -43,4 +50,13 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+	inputs.dir(project.extra["snippetsDir"]!!)
+	dependsOn(tasks.test)
 }
---
+++ src/main/kotlin/no/vingaas/pokermanager/config/FlywayInitializer.kt
@@ -0,0 +1,55 @@
+package no.vingaas.pokermanager.config
+
+import jakarta.annotation.PostConstruct
+import org.flywaydb.core.Flyway
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Component
+import java.sql.DriverManager
+import java.util.concurrent.TimeUnit
+
+@Component
+class FlywayInitializer(private val environment: Environment) {
+    private val logger: Logger = LoggerFactory.getLogger(FlywayInitializer::class.java)
+    @PostConstruct
+    fun migrateDatabase() {
+        logger.info("Starting database migration...")
+        val dbUrl = environment.getRequiredProperty("spring.datasource.url")
+        val dbUser = environment.getRequiredProperty("spring.datasource.username")
+        val dbPass = environment.getRequiredProperty("spring.datasource.password")
+        val dbSchema = environment.getRequiredProperty("DB_SCHEMA")
+        val flywayDbSchema = environment.getRequiredProperty("FLYWAY_DB_SCHEMA")
+        val flywayLocation = environment.getRequiredProperty("FLYWAY_LOCATION")
+
+        val flyway = Flyway.configure()
+            .dataSource(dbUrl, dbUser, dbPass)
+            .locations(flywayLocation)
+            .schemas(flywayDbSchema)
+            .load()
+
+        flyway.migrate()
+        logger.info("Database migration completed.")
+    }
+
+    private fun waitForDatabaseToStart(dbUrl: String, dbUser: String, dbPass: String) {
+        logger.info("Waiting for database to start...")
+        var attempts = 0
+        while (attempts < 30) {
+            try {
+                DriverManager.getConnection(dbUrl, dbUser, dbPass).use { }
+                logger.info("Database is up and running!")
+                return
+            } catch (e: Exception) {
+                attempts++
+                if (attempts > 10) {
+                    logger.warn("Database start attempt exceeded 10 times...")
+                }
+                logger.info("Waiting for database... attempt: $attempts")
+                TimeUnit.SECONDS.sleep(5)
+            }
+        }
+        logger.error("Database did not start after 30 attempts")
+        throw RuntimeException("Database did not start after 30 attempts")
+    }
+}

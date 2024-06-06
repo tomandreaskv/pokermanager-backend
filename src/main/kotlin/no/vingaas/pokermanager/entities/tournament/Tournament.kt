b31e26177b3ac25456a3ad99d375f5ee814c7af4
Tom Andreas Krane Vingås <tom.andreas@vingaas.no>
2024-06-06 09:36:08 +0200

    improved and renamed entities related to Tournament

--- src/main/kotlin/no/vingaas/pokermanager/entities/tournament/Tournament.kt
+++ src/main/kotlin/no/vingaas/pokermanager/entities/tournament/Tournament.kt
@@ -20,43 +20,25 @@ data class Tournament(
     val description: String?,
 
     @ManyToOne
-    @JoinColumn(name = "tournament_type_id", nullable = false)
-    val tournamentType: TournamentType,
-    @Column(name = "buy_in", nullable = false)
-    val buyIn: Double?,
-
-    @Column(name = "free_to_play", nullable = false)
-    val freeToPlay: Boolean,
+    @JoinColumn(name = "specification_id", nullable = false)
+    val specification: TournamentSpecification,
 
     @ManyToOne
     @JoinColumn(name = "status_id", nullable = false)
     val status: Status,
+
     @Column(name = "scheduled_start_time")
     val scheduledStartTime: LocalDateTime?,
+
     @Column(name = "actual_start_time")
     val actualStartTime: LocalDateTime?,
+
     @Column(name = "end_time")
     val endTime: LocalDateTime?,
-    @Column(name = "current_level")
-    val currentLevel: Int?,
-    @Column(name = "remaining_level_time")
-    val remainingLevelTime: Int?,
 
     @ManyToOne
     @JoinColumn(name = "created_by", nullable = false)
     val createdBy: User,
-
-    @ManyToOne
-    @JoinColumn(name = "blind_structure_id")
-    val blindStructure: BlindStructure?,
-
-    @ManyToOne
-    @JoinColumn(name = "starting_stack_id")
-    val startingStack: StartingStack?,
-
-    @ManyToOne
-    @JoinColumn(name = "settings_id")
-    val settings: TournamentSettings?,
 
     @ManyToOne
     @JoinColumn(name = "series_id")
@@ -66,5 +48,20 @@ data class Tournament(
     val createdAt: LocalDateTime,
 
     @Column(name = "updated_at", nullable = false)
-    val updatedAt: LocalDateTime
+    val updatedAt: LocalDateTime,
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_tournaments",
+        joinColumns = [JoinColumn(name = "tournament_id")],
+        inverseJoinColumns = [JoinColumn(name = "user_id")]
+    )
+    val participants: List<User> = mutableListOf(),
+
+    @OneToMany(mappedBy = "tournament")
+    val rounds: List<TournamentRound> = mutableListOf(),
+
+    @OneToOne
+    @JoinColumn(name = "current_round_id")
+    val currentRound: TournamentRound? = null
 )

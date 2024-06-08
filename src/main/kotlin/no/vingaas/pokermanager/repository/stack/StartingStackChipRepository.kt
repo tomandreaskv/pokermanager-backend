a5ebd5815dcbafe0d4cd3b221cc1cd9444143eb2
Tom Andreas Krane Vingås <tom.andreas@vingaas.no>
2024-06-08 22:18:49 +0200

    added repository for notification

--- src/main/kotlin/no/vingaas/pokermanager/repository/stack/StartingStackChipRepository.kt
+++ src/main/kotlin/no/vingaas/pokermanager/repository/stack/StartingStackChipRepository.kt
@@ -2,6 +2,8 @@ package no.vingaas.pokermanager.repository.stack
 
 import no.vingaas.pokermanager.entities.stack.StartingStackChip
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface StartingStackChipRepository : JpaRepository<StartingStackChip, Long> {
 }

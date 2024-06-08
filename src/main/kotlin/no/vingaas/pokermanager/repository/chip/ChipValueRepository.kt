eb23d3ebc6c2dd6975847129ae3269630b39bfff
Tom Andreas Krane Vingås <tom.andreas@vingaas.no>
2024-06-08 22:08:42 +0200

    added anotation to repository interface

--- src/main/kotlin/no/vingaas/pokermanager/repository/chip/ChipValueRepository.kt
+++ src/main/kotlin/no/vingaas/pokermanager/repository/chip/ChipValueRepository.kt
@@ -2,6 +2,8 @@ package no.vingaas.pokermanager.repository.chip
 
 import no.vingaas.pokermanager.entities.chip.ChipValue
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ChipValueRepository : JpaRepository<ChipValue, Long> {
 }

08617f09d482a2d9c4bf88c28553b4d22a0a213c
Tom Andreas Krane Vingås <tom.andreas@vingaas.no>
2024-10-17 19:39:42 +0200

    adding custom userdetails for spring security and to use user id as authentication.principal.id

--- src/main/kotlin/no/vingaas/pokermanager/service/security/CustomUserDetailsServiceImpl.kt
+++ src/main/kotlin/no/vingaas/pokermanager/service/security/CustomUserDetailsServiceImpl.kt
@@ -1,14 +1,16 @@
 package no.vingaas.pokermanager.service.security
 
+import no.vingaas.pokermanager.config.CustomUserDetails
+import no.vingaas.pokermanager.entities.user.User
+import no.vingaas.pokermanager.entities.user.UserCredential
 import no.vingaas.pokermanager.repository.user.UserCredentialRepository
 import no.vingaas.pokermanager.repository.user.UserRepository
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Primary
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
-typealias  ApplicationUser = no.vingaas.pokermanager.entities.user.User
 @Service
 @Primary
 class CustomUserDetailsServiceImpl(
@@ -18,20 +20,13 @@ class CustomUserDetailsServiceImpl(
     private val logger = LoggerFactory.getLogger(CustomUserDetailsServiceImpl::class.java)
     override fun loadUserByUsername(username: String): UserDetails {
         logger.info("Loading user by email: $username")
-        return userRepository.findByEmail(username)
-            ?.mapToUserDetails()
-            ?: throw IllegalArgumentException("User not found")
+        val user: User = userRepository.findByEmail(username)
+            ?: throw UsernameNotFoundException("User not found with email: $username")
+
+        // Finn UserCredential basert på brukerens ID
+        val userCredential: UserCredential = userCredentialRepository.findByUserId(user.id)
+            .orElseThrow { IllegalArgumentException("UserCredential not found for user with id ${user.id}") }
+
+        return CustomUserDetails(user, userCredential)
     }
-
-    private fun ApplicationUser.mapToUserDetails(): UserDetails {
-        logger.info("Mapping user to user details")
-        val userCredential =    userCredentialRepository.findByUserId(this.id).get()
-
-        return User.builder()
-            .username(this.email)
-            .password(userCredential.password)
-            .roles(this.role.name)
-            .build()
-    }
-
 }
